068ff22de7641adc7f488f12a2351ba599bc644f
Vaibhavdimri78 <111555559+Vaibhavdimri78@users.noreply.github.com>
2022-08-19 01:44:11 -0700

    Add files via upload

---
+++ d.py
@@ -0,0 +1,26 @@
+'''#1
+#n1= input("Sir Enter your name please\n")
+print(n1)
+
+#2&3&4
+r=float(input("enter the radius of circle"))
+print("The area of circle is %f",3.14*r**2)
+
+#5
+n1=float(input("enter the number whoes square is required\n"))
+print("square of number is %f",n1**2)
+
+
+#6
+print("eneter the numbers")
+n1=float(input("first number"))
+n2=float(input("second number"))
+n3=float(input("third number"))
+print("the avg of numbers is ",(n1+n2+n3)/3)
+'''
+#10
+l=float(input("Enter the length of rectangle\n"))
+b=float(input("enter the width of rectangle\n"))
+print("The are of rectangle is ",l*b)
+
+
